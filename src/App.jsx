ee228458f0ccdf0ddbc9913908d7f986ca13288e
TI Sabbir <tisabbir0@gmail.com>
2024-03-15 19:15:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1 className="text-6xl bg-red-400">Knowledge Cafe</h1>
+    </>
+  );
+}
+
+export default App;
